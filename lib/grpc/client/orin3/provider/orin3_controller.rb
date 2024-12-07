ed8bbabd2fd0337720466f0ab4d3a5495a9c7488
kkikzk <kkikzk@gmail.com>
2024-12-07 17:00:40 +0900

    実装中

--- lib/grpc/client/orin3/provider/orin3_controller.rb
+++ lib/grpc/client/orin3/provider/orin3_controller.rb
@@ -45,6 +45,22 @@ module Grpc
             argument = argument.nil? ? {} : argument
             close(argument)
           end
+
+          def create_controller(name, type_name, option)
+            begin
+              controller = O3::ControllerCreatorService::Stub.new(nil, :this_channel_is_insecure, channel_override: @channel)
+              request = O3::CreateControllerRequest.new(common: O3P::CommonRequest.new, parent_id: @internal_id, name: name, type_name: type_name, option: option)
+              response = controller.create_controller(request)
+              if (response.common.result_code != :SUCCEEDED)
+                raise MessageClientError.new(response.common.result_code, response.common.detail)
+              end
+              return ORiN3Controller.new(@channel, response.id, response.created_datetime)
+            rescue MessageClientError
+              raise
+            rescue StandardError => e
+              raise MessageClientError.new(e)
+            end
+          end
         end
       end
     end
